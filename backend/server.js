7ecd0a54b08bfebb955c5cd858236c17cb955559
Pawani <pawanibhardwaj13@gmail.com>
2026-08-06 12:07:47 +0530

    Serve frontend with Express

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const userRoutes = require("./routes/userRoutes");
 const dietRoutes = require("./routes/dietRoutes");
@@ -9,9 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>res.send("Backend running"));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.use("/api", userRoutes);
 app.use("/api", dietRoutes);
 
-app.listen(5000, ()=>console.log("Server running on 5000"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server running");
+});
